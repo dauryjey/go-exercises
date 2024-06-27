7962d2c877661edfa71ff17da8a4f8f7090e9840
Daury P. <dauryjey@gmail.com>
2024-06-26 23:10:04 -0400

    chore: Refactor TODO CLI menu options and task handling

--- todo-cli/todo-cli.go
+++ todo-cli/todo-cli.go
@@ -5,42 +5,51 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to TODO CLI")
-	fmt.Println("Please select an option")
-	fmt.Println("1. Add new task")
-	fmt.Println("2. List all tasks")
-	fmt.Println("3. Delete task")
-	fmt.Println("4. Update task")
-	fmt.Println("5. Exit")
+	var tasks []string
+	for {
+		fmt.Println("Welcome to TODO CLI")
+		fmt.Println("Please select an option")
+		fmt.Println("1. Add new task")
+		fmt.Println("2. List all tasks")
+		fmt.Println("3. Delete task")
+		fmt.Println("4. Update task")
+		fmt.Println("5. Exit")
 
-	var option int
+		var option int
 
-	fmt.Scan(&option)
+		fmt.Scan(&option)
 
-	switch option {
-	case 1:
-		addNewTask()
+		switch option {
+		case 1:
+			var newTask string
+			fmt.Println("Enter the task you want to add")
+			fmt.Scan(&newTask)
+			tasks = addNewTask(newTask, tasks)
 
-	case 2:
-		listAllTasks()
+		case 2:
+			listAllTasks(tasks)
 
-	case 3:
-		deleteTask()
+		case 3:
+			deleteTask()
 
-	case 4:
-		updateTask()
+		case 4:
+			updateTask()
 
-	case 5:
-		fmt.Println("Exiting the application")
+		case 5:
+			fmt.Println("Exiting the application")
+			return
+		}
 	}
 }
 
-func addNewTask() {
-	fmt.Println("Add new task")
+func addNewTask(task string, tasks []string) []string {
+	return append(tasks, task)
 }
 
-func listAllTasks() {
-	fmt.Println("List all tasks")
+func listAllTasks(tasks []string) {
+	for i, task := range tasks {
+		fmt.Println("TaskID:", i+1, "Task:", task)
+	}
 }
 
 func deleteTask() {
